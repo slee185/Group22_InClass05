2509ee0503323b32fc76dc790a6e5c2110c8892a
Ken Stanley <dohpaz@gmail.com>
2022-09-20 20:58:31 -0400

    Remove unused imports

--- app/src/main/java/edu/uncc/inclass05/fragments/AppCategoriesFragment.java
+++ app/src/main/java/edu/uncc/inclass05/fragments/AppCategoriesFragment.java
@@ -14,7 +14,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
